afb0c46bdd49b7f6fe8b03edc54188c23052c0d4
bllakas <kkmbll.a@gm.gist.ac.kr>
2022-11-16 11:54:46 +0900

    test

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print("something")
-print("test")
+def shout(text):
+    return text.upper()
+def whisper(text):
+    return text.lower()
+def greet(func):
+    greeting = func("Hello World")
+    print(greeting)
+greet(shout)
+greet(whisper)
