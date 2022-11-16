bf7eea9e19b6a4c2e12c8abb76e6407af770b0c5
Kornkamol Anasart <92564809+bllakas@users.noreply.github.com>
2022-11-16 11:55:55 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-def shout(text):
-    return text.upper()
-def whisper(text):
-    return text.lower()
-def greet(func):
-    greeting = func("Hello World")
-    print(greeting)
-greet(shout)
-greet(whisper)
