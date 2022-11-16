177b0fb5e5f631d266209f68e98afcb6544baa59
bllakas <kkmbll.a@gm.gist.ac.kr>
2022-11-16 13:45:29 +0900

    modified

--- protocol.py
+++ protocol.py
@@ -1,12 +1,14 @@
 from enum import Enum, auto
 
-#Client protocol FOR NOW
+#Client protocol
 #  data = {
 #     'mode': 'login',
 #     'client_id': 'client1',
 #     'logined': True,
 #     'requested': True,
 #     'sent': True, #there is receiverID in customer list
+#     'receiver_id': 'client2',
+#     'messages': 'date\\text\\hello | date\\<type of msg, image, text, video>\\hi...',
 # }
 # send_msg = json.dumps(data) # convert dict to str by json
 # read data by json.loads()
