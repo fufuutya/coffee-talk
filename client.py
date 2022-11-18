7474083cc1b6f757ffb8d7f0f4a5c8f250c5a9f5
bllakas <kkmbll.a@gm.gist.ac.kr>
2022-11-18 17:51:16 +0900

    UPDATE client.py

--- client.py
+++ client.py
@@ -51,41 +51,57 @@ def send():
             client_id = input('Enter your ID: ')
             send_msg = json.dumps({'mode': mode, 'client_id': client_id})
             client.sendall(send_msg.encode())
-            if recv_msg['logined'] == True:
-                print('Login success')
-                print('Welcome', recv_msg['id'])
-                while recv_msg['logined']:
-                    mode = input('Enter mode(send, request, exit): ')
-                    if mode == 'send':
-                        sending = True
-                        while sending:
-                            receiver_id = input('Enter receiver ID: ')
-                            message = input('Enter message: ')
-                            send_msg = json.dumps({'mode': mode, 'client_id': client_id, 'receiver_id': receiver_id, 'message': message})
-                            client.sendall(send_msg.encode())
-                            if recv_msg['sent'] == True:
-                                print('Message sent')
-                            else:
-                                print('Message not sent due to invalid receiver ID')
-                                sending = convert_yesno_to_bool(input('Do you want to send message again? (y/n): '))
-                            sending = convert_yesno_to_bool(input('Do you want to send another message? (y/n): '))
-                    elif mode == 'request':
-                        requesting = True
-                        while requesting:
-                            send_msg = json.dumps({'mode': mode, 'client_id': client_id})
-                            client.sendall(send_msg.encode())
-                            if recv_msg['requested'] == True:
-                                print('Request success')
-                                print('Messages from', recv_msg['id'])
-                                print(recv_msg['messages'])
-                            else:
-                                print('Request failed')
-                            requesting = convert_yesno_to_bool(input('Do you want to request again? (y/n): '))
+            login_mode = True
+            while login_mode:
+                if recv_msg['logined'] == True:
+                    print('Login success')
+                    print('Welcome', recv_msg['id'])
+                    while recv_msg['logined']:
+                        mode = input('Enter mode(send, request): ')
+                        if mode == 'send':
+                            sending = True
+                            while sending:
+                                receiver_id = input('Enter receiver ID: ')
+                                message = input('Enter message: ')
+                                send_msg = json.dumps({'mode': mode, 'client_id': client_id, 'receiver_id': receiver_id, 'message': message})
+                                client.sendall(send_msg.encode())
+                                if recv_msg['sent'] == True:
+                                    print('Message sent')
+                                else:
+                                    print('Message not sent due to invalid receiver ID')
+                                    sending = convert_yesno_to_bool(input('Do you want to send message again? (y/n): '))
+                                sending = convert_yesno_to_bool(input('Do you want to send another message? (y/n): '))
+                        elif mode == 'request':
+                            requesting = True
+                            while requesting:
+                                send_msg = json.dumps({'mode': mode, 'client_id': client_id})
+                                client.sendall(send_msg.encode())
+                                if recv_msg['requested'] == True:
+                                    print('Request success')
+                                    print('Messages from', recv_msg['id'])
+                                    print(recv_msg['messages'])
+                                else:
+                                    print('Request failed')
+                                requesting = convert_yesno_to_bool(input('Do you want to request again? (y/n): '))
+                elif recv_msg['logined'] == False:
+                    print('Login failed')
+                    login_mode = convert_yesno_to_bool(input('Do you want to login again? (y/n): '))
                             
         elif mode == 'register':
             client_id = input('Enter your ID: ')
             send_msg = {'mode': mode, 'id': client_id}
             client.sendall(send_msg.encode())
+            register_mode = True
+            while register_mode:
+                if recv_msg['registered'] == True:
+                    print('Register success')
+                    print('Welcome', recv_msg['id'])
+                    register_mode = False
+                if recv_msg['registered'] == False:
+                    print('Register failed')
+                    print('ID already exist')
+                    register_mode = convert_yesno_to_bool(input('Do you want to register again? (y/n): '))
+            continue
         elif mode == 'exit':
             print('Goodbye')
             sys.exit()
